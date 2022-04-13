74a3ad0d1ae5d6266a32cdb90574bb1de6a0ea85
unknown <n.kravchenko@sangu.edu.ge>
2022-04-13 11:42:15 +0400

    Implemented RemoveAction's performAction method

--- src/ge/edu/sangu/action/RemoveAction.java
+++ src/ge/edu/sangu/action/RemoveAction.java
@@ -1,9 +1,23 @@
 package ge.edu.sangu.action;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class RemoveAction implements IAction {
 
     @Override
     public void performAction(String... args) {
-        throw new RuntimeException("Not implemented yet");
+        for (String fileOrFolderPath : args) {
+            Path path = Paths.get(fileOrFolderPath);
+            if (Files.isRegularFile(path)) {
+                try {
+                    Files.deleteIfExists(path);
+                } catch (IOException e) {
+                    System.err.println("Couldn't delete file: " + path + ", reason: " + e.getMessage());
+                }
+            }
+        }
     }
 }
